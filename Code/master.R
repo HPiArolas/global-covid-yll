69c2e747a1d11592c304dda3086795c3bd0d4a4c
Tim Riffe <tim.riffe@gmail.com>
2020-06-17 17:46:43 +0200

    master script

---
+++ Code/master.R
@@ -0,0 +1,44 @@
+
+
+###################################################################################################
+##
+##  LIBRARIES
+##  
+###################################################################################################
+
+# All of these packages are on CRAN
+library(tidyverse)
+library(ggplot2)
+library(hrbrthemes)
+library(viridis)
+library(wpp2019)
+library(readr)
+library(countrycode)
+library(paletteer) 
+library(grid)
+library(here)
+
+# 1) 
+source(here("Code","B - Excess YLL.R"))
+
+# 2)
+source(here("Code","B - Other Causes YLL.R"))
+
+# 3)
+source(here("Code","B - Projected YLL.R"))
+
+# 4)
+source(here("Code","C - Compilation final measures.R"))
+
+# 5)
+source(here("Code","C - Figures.R"))
+
+# 6)
+source(here("Code","C - Final tables.R"))
+
+
+
+
+
+
+
