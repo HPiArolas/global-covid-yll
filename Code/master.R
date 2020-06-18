e9e4f0ec95c9d8ebe83e779a6892c2101401924d
Enrique Acosta <kikepaila@gmail.com>
2020-06-18 08:10:33 +0200

    Update master.R
    added two scripts and all used libraries in them
    detach(package:MASS) after using it because of conflict with tidyverse select

--- Code/master.R
+++ Code/master.R
@@ -17,23 +17,38 @@ library(countrycode)
 library(paletteer) 
 library(grid)
 library(here)
+library(lubridate)
+library(readxl) 
+library(doParallel) 
+library(foreach)
+library(stats)
+library(splines)
+library(MASS)
+library(gnm)
 
 # 1) 
+source(here("Code","A - 1_Mortality_baseline.R"))
+detach(package:MASS)
+
+# 2) 
+source(here("Code","A - 2_Excess_mortality_2020.R"))
+
+# 3) 
 source(here("Code","B - Excess YLL.R"))
 
-# 2)
+# 4)
 source(here("Code","B - Other Causes YLL.R"))
 
-# 3)
+# 5)
 source(here("Code","B - Projected YLL.R"))
 
-# 4)
+# 6)
 source(here("Code","C - Compilation final measures.R"))
 
-# 5)
+# 7)
 source(here("Code","C - Figures.R"))
 
-# 6)
+# 8)
 source(here("Code","C - Final tables.R"))
 
 
